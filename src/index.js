d6a5534c3397f67ff598597f9d56eff5fd897c2b
Paweł Szlendak <kontakt@pawelszlendak.pl>
2023-07-22 18:52:49 +0200

    changes

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@
  * This code is released under the MIT license.
  */
 
-import React, { useRef } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.scss';
